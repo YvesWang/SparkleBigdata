15b8658ae85dade2c7133ef4441a72ef029e17a5
Tianyu Wang <yvestyw@gmail.com>
2018-04-16 14:17:30 -0400

    add commands in spark.py

--- spark.py
+++ spark.py
@@ -2,8 +2,33 @@
 from csv import reader
 import sys
 from pyspark import SparkContext, SparkConf
+from pyspark.sql import Row
+
 conf = SparkConf().setMaster("local").setAppName("Test")
-sc = SparkContext(conf=conf)
-def search(query):
-	text = sc.textFile("/user/ys3225/1004project/result0.out",200)
-	result = text.filter(lambda x: x.startswith(query)).filter(lambda x: x.split('\t|:|;')[])
+
+text = sc.textFile("/user/tw1682/Bigdata/Project/result0",200)
+textrdd = text.map(lambda x: Row(Key=x.split('\t')[0],Filename_Count=x.split('\t')[1]))
+textframe = spark.createDataFrame(textrdd)
+
+def search_startwith(query):
+	return textframe.filter(textframe['Key'].startswith(query))
+
+
+def search_accuracy(query):
+	return textframe.filter(textframe['Key'] == query)
+
+
+def search_contain(query):
+	return textframe.filter(textframe['Key'].like('%'+query+'%'))
+
+
+#multi query command
+def search(querys):
+	for query in querys:
+		if call_startwith:
+			textframe=textframe.filter(textframe['Key'].startswith(query))
+		elif call_accuracy:
+			textframe = textframe.filter(textframe['Key'] == query)
+		elif call_contain:
+			textframe = textframe.filter(textframe['Key'].like('%'+query+'%'))
+	return textframe
