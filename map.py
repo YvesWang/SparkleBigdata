a80e87e60b805d9338cb436186394823e5b15e8b
Yu Sun <ys3225@login-1-1.local>
2018-04-25 17:57:22 -0400

    remain comma inside string

--- map.py
+++ map.py
@@ -3,18 +3,18 @@
 import os
 import io
 import sys
-
+import csv
 stopword = [":",",",""," ","[","]","\n","{","}"]
 
 filename = os.environ.get("mapreduce_map_input_file")
-for i in sys.stdin:
-    if i.startswith('  "data"'):
-        for j in i.split(",")[1:]:
-            j=j.strip(''.join(stopword))
+for line in sys.stdin:
+    if line.startswith('  "data"'):
+        for j in csv.reader(line.strip()):
+            j=str(j[0]).strip(''.join(stopword))
             if j not in stopword:
                 print('{0:s}\t{1:s},{2:d}'.format(j,filename,1))
-    elif i.startswith(', [ '):
-        for j in i.split(","):
-            j=j.strip(''.join(stopword))
+    elif line.startswith(', [ '):
+        for j in csv.reader(line.strip()):
+            j=str(j[0]).strip(''.join(stopword))
             if j not in stopword:
                 print('{0:s}\t{1:s},{2:d}'.format(j,filename,1))
