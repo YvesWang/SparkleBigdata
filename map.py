dc07003d3120826efb776e13c08633d9ea6fbc99
Yu Sun <ys3225@login-1-1.local>
2018-04-25 18:27:13 -0400

    transfer each word to lower

--- map.py
+++ map.py
@@ -10,11 +10,11 @@ filename = os.environ.get("mapreduce_map_input_file")
 for line in sys.stdin:
     if line.startswith('  "data"'):
         for j in csv.reader(line.strip()):
-            j=str(j[0]).strip(''.join(stopword))
+            j=str(j[0]).strip(''.join(stopword)).lower()
             if j not in stopword:
                 print('{0:s}\t{1:s},{2:d}'.format(j,filename,1))
     elif line.startswith(', [ '):
         for j in csv.reader(line.strip()):
-            j=str(j[0]).strip(''.join(stopword))
+            j=str(j[0]).strip(''.join(stopword)).lower()
             if j not in stopword:
                 print('{0:s}\t{1:s},{2:d}'.format(j,filename,1))
